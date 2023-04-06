37f36951b8f3ce8d64968b4701c83c37d9348872
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-06 09:51:25 +0530

    6 april program 3

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-int main(){
-    struct st{
-        int a;
-        int b;
-    };
-    struct st st1={0};
-    printf("%d",st1.a);
-    printf("%d",st1.b);
-    return 0;
+struct student{
+    char name[20];
+    int age;
+    int mobile ;
+    int regid;
+    float cgpa;
+};
+void main()
+{
+    struct student st1;
+    printf("Enter the name: ");
+    scanf("%s",&st1.name);
+    printf("Enter the age: ");
+    scanf("%d",&st1.age);
+    printf("Enter the mobile number: ");
+    scanf("%d",&st1.mobile);
+    printf("Enter the regid: ");
+    scanf("%d",&st1.regid);
+    printf("Enter the cgpa: ");
+    scanf("%f",&st1.cgpa);
+    printf("%s %d %d %f",st1.name,st1.age,st1.mobile,st1.cgpa);
 }
+
