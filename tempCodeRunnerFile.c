a76f6398e59c645f17a73a36cb5147c6a67fba2e
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-06 09:47:41 +0530

    6 april program 2

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-struct car{
-    char name[20];
-    int seats;
-    float price;
-};
-void main(){
-    struct car mycar;
-    printf("\nEnter car name of car: ");
-    scanf("%s",&mycar.name);
-    printf("\nEnter number of seats: ");
-    scanf("%d",&mycar.seats);
-    printf("\nEnter price: ");
-    scanf("%f",&mycar.price);
-    printf("%s %d %f",mycar.name,mycar.seats,mycar.price);
-    printf("\nThe memory size of my car variable is: %d",sizeof(mycar));
+int main(){
+    struct st{
+        int a;
+        int b;
+    };
+    struct st st1={0};
+    printf("%d",st1.a);
+    printf("%d",st1.b);
     return 0;
 }
