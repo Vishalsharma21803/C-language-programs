db554c7f36afff6daeec526277ae1404b921ced0
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-12 05:54:54 +0000

    Wednesday, April 12, 2023, 11:24 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int var1=60,var2=70;
-    int *constant ptr=&var1;
+    int *const ptr=&var1;
     printf("\n%d",*ptr);
     ptr=&var2;  // invalid errot will rise
     printf("\n%d",*ptr);
