f253912e2dc584227d300e0183b3215cff97ffb1
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-13 05:25:16 +0000

    Thursday, April 13, 2023, 10:55 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,21 +1,3 @@
-
-#include <stdio.h>
-
-int main() {
-int x = 10, y = 20;
-int *p, *q; 
-
-p = &x;
-q = &y;
-
-(*p > *q) ? printf("%d is greater than %d\n", *p, *q) : printf("%d is greater than %d\n", *q, *p);
-
-return 0;
-}
-
-//__________________________________________________
-
-
 #include <stdio.h>
 int main()
 {
