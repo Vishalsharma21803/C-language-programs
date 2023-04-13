d31258674507a280a2d0e90aa22199e828aaaf92
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-13 05:25:04 +0000

    Thursday, April 13, 2023, 10:55 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,34 @@
-#include <stdio.h>
-int main(){
-    int *ptr;
-    {
-        int var=23;
-        ptr=&var;
-        printf("\n%d",*ptr); // 23 is printed
-        printf("\n%u",ptr); // address of var is printed
 
-    }
-    printf("\n%u",ptr); // same address is printed, even var is destroyed, hence ptr is dangling pointer
-    return 0;
+#include <stdio.h>
+
+int main() {
+int x = 10, y = 20;
+int *p, *q; 
+
+p = &x;
+q = &y;
+
+(*p > *q) ? printf("%d is greater than %d\n", *p, *q) : printf("%d is greater than %d\n", *q, *p);
+
+return 0;
+}
+
+//__________________________________________________
+
+
+#include <stdio.h>
+int main()
+{
+int a[] = {1, 2, 3, 4};
+int *p1 = a, i;
+int *p2 = &a[2];
+p2--;
+*(p2 - 1) = 90;
+p1 = p2;
+*p1 = 100;
+for (i = 0; i < 4; i++)
+{
+printf("%d\n", a[i]);
+}
+return 0;
 }
