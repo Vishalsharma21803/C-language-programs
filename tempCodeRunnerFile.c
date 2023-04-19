fcd20b035924b15db61a5bdd3759d25dcc8c6142
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-19 05:55:57 +0000

    Wednesday, April 19, 2023, 11:25 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,22 +1,10 @@
-//__________________________________________________________________
-
-
 #include <stdio.h>
-int main()
-{
-FILE *file;
-char str[500];
-if (file = fopen("FileHandling01.txt", "r"))
-{
-while (fscanf(file, "%s", str) != EOF)  // fscanf - used to read the file not entering the values
-{
-printf("%s", str);
-}
-}
-else
-{
-printf("Error in opening file");
-}
-fclose(file);
-return 0;
+int main(){
+    FILE *file;
+    char str[500];
+    if (file = fopen("hello.txt", "r")){
+        printf("%s", fgets(str,10,file));
+    }
+    fclose(file);
+    return 0;
 }
