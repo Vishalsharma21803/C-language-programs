d8d3164956b9690327fea7b0bcbb40af48cc14b3
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-19 05:09:53 +0000

    Wednesday, April 19, 2023, 10:39 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-int main(){
-    FILE *file;  // FILE - datatype     file - variable
-    if (file = fopen("vishal.txt","r")){
-        printf("file opened successfully in read mode");
-}
-else{
-    printf("the file is not present");
+int main()
+{
+    FILE *file; // FILE - datatype     file - variable
+    if (file = fopen("hello.txt", "a"))
+    {
+        if (fputs("he was a great personality.....", file) >= 0)
+        {
+            printf("file opened successfully in append mode and new content has been added to the old content");
+        }
+    }
+    else
+    {
+        printf("Error!");
     }
     fclose(file);
     return 0;
-    }
+}
