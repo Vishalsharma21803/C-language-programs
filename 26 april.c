b94347f76d01a0528351d2420905e9a9d090174c
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 05:46:22 +0000

    Wednesday, April 26, 2023, 11:16 AM India Standard Time

--- 26 april.c
+++ 26 april.c
@@ -57,3 +57,24 @@ int main(){
 
 //_______________________________________________________
 
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[15],str2[15];
+    int n;
+    printf("\nEnter source string: ");
+    gets(str1);
+    printf("\nEnter destination string: ");
+    gets(str2);
+    printf("\nEnter number of characters to copy in destination string: ");
+    scanf("%d",&n);
+    strncpy(str2,str1,n);  // jitne n ki value dalenge utne letters str1 se lekar str2 ke n letters ki jagah daal dega 
+    printf("\nsource string is: %s",str1);
+    printf("\ndestination string is: %s",str2);
+    return 0;
+  
+}
+
+//_______________________________________________________
+
+
