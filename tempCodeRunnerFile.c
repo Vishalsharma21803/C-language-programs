71e4a7b5c0e926fb178b03ed143078817b488085
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 06:09:47 +0000

    Wednesday, April 26, 2023, 11:39 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,19 +2,20 @@
 #include <string.h>
 int main(){
     char str1[20],str2[20];
-    int x;
+    int n;
     printf("\nEnter first string: ");
     gets(str1);
     printf("\nEnter second string: ");
     gets(str2);
-    x=strcmp(str1,str2);  // comparing the two strings
-    if(x==0)
+    printf("\nEnter the number of characters you want to compare from str2 with str1: ");
+    scanf("%d",&n);
+    strncmp(str1,str2,n);  // comparing n characters the two strings
+    if(n==0)
     printf("\nThe two strings are equal");
-    else if(x<0)
+    else if(n<0)
     printf("\nThe second string is greate than the first");
     else
     printf("\nThe first string is greater than the second");
     return 0;
-
-
+    
 }
