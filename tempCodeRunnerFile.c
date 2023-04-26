424ff6655671141fb8562d161e45b36097ec1129
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 05:48:32 +0000

    Wednesday, April 26, 2023, 11:18 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,16 +2,11 @@
 #include <string.h>
 int main(){
     char str1[15],str2[15];
-    int n;
-    printf("\nEnter source string: ");
+    printf("\nEnter first string: ");
     gets(str1);
-    printf("\nEnter destination string: ");
+    printf("\nEnter second string: ");
     gets(str2);
-    printf("\nEnter number of characters to copy in destination string: ");
-    scanf("%d",&n);
-    strncpy(str2,str1,n);
-    printf("\nsource string is: %s",str1);
-    printf("\ndestination string is: %s",str2);
+    srecat(str1,str2)
+    printf("\nThe concatenation of the two strings is: %s",str1);
     return 0;
-  
 }
