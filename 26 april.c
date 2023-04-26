591a188eecb0140fdf4bca4aa6d85316ae28f82a
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 05:21:44 +0000

    Wednesday, April 26, 2023, 10:51 AM India Standard Time

--- 26 april.c
+++ 26 april.c
@@ -34,3 +34,26 @@ int main(){
 }
 
 //_______________________________________________________________
+
+
+// strcpy function
+
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char ori[20],dup[20];
+    char *z;
+    printf("\nEnter your name: ");
+    gets(ori);
+    printf("\nEnter your name again: ");
+    gets(dup);
+    z=strcpy(ori,dup); // original ki value ko overright kar kar rahe h duplicate ki value se. original ki value duplicate ki value ke same ho jayegi
+    printf("\nThe original string now is: %s",ori);
+    printf("\nThe duplicate string is: %s",dup);
+    printf("\nThe value of z is: %s",z);
+    return 0;
+}
+
+//_______________________________________________________
+
