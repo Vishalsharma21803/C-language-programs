c680621fb114d86fd06355a5e20c9d19250687ad
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 05:44:12 +0000

    Wednesday, April 26, 2023, 11:14 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char ori[20],dup[20];
-    char *z;
-    printf("\nEnter your name: ");
-    gets(ori);
-    printf("\nEnter your name again: ");
-    gets(dup);
-    z=strcpy(ori,dup); // original ki value ko overright kar kar rahe h duplicate ki value se. original ki value duplicate ki value ke same ho jayegi
-    printf("\nThe original string now is: %s",ori);
-    printf("\nThe duplicate string is: %s",dup);
-    printf("\nThe value of z is: %s",z);
+    char str1[15],str2[15];
+    int n;
+    printf("\nEnter source string: ");
+    gets(str1);
+    printf("\nEnter destination string: ");
+    gets(str2);
+    printf("Enter number of characters to copy in destination string: ");
+    scanf("%d",&n);
+    strncpy(str2,str1,n);
+    printf("source string is: %s",str1);
+    printf("destination string is: %s",str2);
     return 0;
+  
 }
