f39537a2ae602be99d6abb410055875fa9959b85
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 05:03:49 +0000

    Wednesday, April 26, 2023, 10:33 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-int main(){
-    int *ptr;
-    {
-        int var=23;
-        ptr=&var;
-        printf("\n%d",*ptr); // 23 is printed
-        printf("\n%u",ptr); // address of var is printed
+void main(){
+    char carname[20]; 
+    printf("Enter the name of the car: ");
+    scanf("%s", carname);
+    printf("The name of the car is %s", carname);
 
-    }
-    printf("\n%u",ptr); // same address is printed, even var is destroyed, hence ptr is dangling pointer
-    return 0;
 }
