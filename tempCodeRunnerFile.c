c4145d4c212b3db400bc949e70dfdfe66b52f204
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 05:04:28 +0000

    Wednesday, April 26, 2023, 10:34 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-void main(){
-    char carname[20]; 
-    printf("Enter the name of the car: ");
-    scanf("%s", carname);
-    printf("The name of the car is %s", carname);
-
+int main(){
+    char ary[]="discovery channel";
+    printf("%s",ary);
+    return 0;
 }
