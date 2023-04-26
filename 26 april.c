035d14ae3cb77bbdfbc100a94ebbcad44d8d6d1d
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 05:54:30 +0000

    Wednesday, April 26, 2023, 11:24 AM India Standard Time

--- 26 april.c
+++ 26 april.c
@@ -94,4 +94,23 @@ int main(){
 
 //_____________________________________________________________
 
+// strncat
 
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[20],str2[20];
+    int n;
+    printf("\nEnter first string: ");
+    gets(str1);
+    printf("\nEnter second string: ");
+    gets(str2);
+    printf("\nEnter the number of letters you want to contaninate from str2 with str1: ");
+    scanf("%d",&n);
+    strncat(str1,str2,n);  // str2 se n letters lekar str1 mein join(add) kar raha hai
+    printf("\nThe concatenation of the two strings is: %s",str1);
+    return 0;
+}
+
+
+//__________________________________________________________________
