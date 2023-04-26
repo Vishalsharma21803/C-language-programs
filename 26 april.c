efc975486b975d6279576c94b1b46c2ca043d4ff
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 06:07:34 +0000

    Wednesday, April 26, 2023, 11:37 AM India Standard Time

--- 26 april.c
+++ 26 april.c
@@ -114,3 +114,28 @@ int main(){
 
 
 //__________________________________________________________________
+
+
+// strcmp  ---- to compare two strings
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[20],str2[20];
+    int x;
+    printf("\nEnter first string: ");
+    gets(str1);
+    printf("\nEnter second string: ");
+    gets(str2);
+    x=strcmp(str1,str2);  // comparing the two strings
+    if(x==0)
+    printf("\nThe two strings are equal");
+    else if(x<0)
+    printf("\nThe second string is greate than the first");
+    else
+    printf("\nThe first string is greater than the second");
+    return 0;
+}
+
+//____________________________________________________________
+
