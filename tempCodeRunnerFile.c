48a07523cd8476f77653d609586cefa132c66230
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 06:02:06 +0000

    Wednesday, April 26, 2023, 11:32 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,21 @@
+
 #include <stdio.h>
 #include <string.h>
 int main(){
     char str1[20],str2[20];
-    int n;
+    int x;
     printf("\nEnter first string: ");
     gets(str1);
     printf("\nEnter second string: ");
     gets(str2);
-    printf("\nEnter the number of letters you want to contaninate from str2 with str1: ");
-    scanf("%d",&n);
-    strncat(str1,str2,n);  // str1 and str2 ko jod(add) jkat str1 mein overweite kar raha hai
-    printf("\nThe concatenation of the two strings is: %s",str1);
+    x=strcmp(str1,str2);  // comparing the two strings
+    if(x==0)
+    printf("\nThe two strings are equal");
+    else if(x<0)
+    printf("\nThe first string is smaller than the second");
+    else
+    printf("\nThe first string is greater than the second");
     return 0;
+
+
 }
