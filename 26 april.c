7ffe4caf05188150c49bcb6219f902d20a8e8d5e
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-26 06:11:26 +0000

    Wednesday, April 26, 2023, 11:41 AM India Standard Time

--- 26 april.c
+++ 26 april.c
@@ -139,3 +139,28 @@ int main(){
 
 //____________________________________________________________
 
+// strncmp  --- number of characters you want to compare
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[20],str2[20];
+    int n;
+    printf("\nEnter first string: ");
+    gets(str1);
+    printf("\nEnter second string: ");
+    gets(str2);
+    printf("\nEnter the number of characters you want to compare from str2 with complete str1: ");
+    scanf("%d",&n);
+    strncmp(str1,str2,n);  // comparing n characters the two strings
+    if(n==0)
+    printf("\nThe two strings are equal");
+    else if(n<0)
+    printf("\nThe second string is greate than the first");
+    else
+    printf("\nThe first string is greater than the second");
+    return 0;
+    
+}
+
+//____________________________________________________________
