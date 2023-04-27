7d90d57846360aa3ddc50d7e19602f7dc28ed692
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 04:08:07 +0000

    Thursday, April 27, 2023, 9:38 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,15 @@
-
-
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char str1[20],str2[20];
+    char str1[20];
     printf("Enter thr str1: ");
     gets(str1);
-    int i=0;
+    int i=0,temp=0;
     while(str1[i]!='\0'){
-        str1[i]=str2[i];
+        temp=temp+1;
         i++;
     }
-    printf("\nnew copied str1 is: %c",str1);
+    printf("\nlength of str1 is: %d",temp);
     return 0;
 
 }
