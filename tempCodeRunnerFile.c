121efafa17d6ce10df2e6fa9e507ebe6b1acfedf
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 04:03:50 +0000

    Thursday, April 27, 2023, 9:33 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,8 @@
-
-
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char str1[20]="vishal";
-    char str2[20]="sharma";
+    char str1[6]="vishal";
+    char str2[6]="sharma";
     int i=0;
     while(str1[i]!='\0'){
         str1[i]=str2[i];
