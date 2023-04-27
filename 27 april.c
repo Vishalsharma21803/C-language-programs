6e7d773e8d1c533f8acef6a441130b139ddf4063
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 03:56:03 +0000

    Thursday, April 27, 2023, 9:26 AM India Standard Time

--- 27 april.c
+++ 27 april.c
@@ -28,3 +28,18 @@ int main(){
 
 //__________________________________________________________
 
+// strlwr  -- to convert uppercase to lowercase  (converting capital letters to small letters)
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[20];
+    printf("Enter the string in highercase: ");
+    gets(str);
+    printf("the string in lowercase is: %s",strlwr(str));
+    return 0;
+
+}
+
+//__________________________________________________________
+
