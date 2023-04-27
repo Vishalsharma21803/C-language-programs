42678f0ac080010bca83c655f585ab1159e5a716
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 04:10:55 +0000

    Thursday, April 27, 2023, 9:40 AM India Standard Time

---
+++ 27 april part 2.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str1[20];
+    printf("Enter thr str1: ");
+    gets(str1);
+    int i = 0, temp = 0;
+    while (str1[i] != '\0')
+    {
+        temp = temp + 1;
+        i++;
+    }
+    printf("\nlength of str1 is: %d", temp);
+    return 0;
+}
+
+//________________________________________________
