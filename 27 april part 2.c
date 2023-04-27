7d42050959576bfd962f4b3a8b5e6e68b97d1572
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 04:17:37 +0000

    Thursday, April 27, 2023, 9:47 AM India Standard Time

--- 27 april part 2.c
+++ 27 april part 2.c
@@ -18,3 +18,49 @@ int main()
 }
 
 //________________________________________________
+
+
+// CONVERTING UPPERCASE TO LOWERCASE WITHOUT USING ANY INBUILT FUNCTION
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[20];
+    char str2[20];
+    printf("Enter str1: ");
+    gets(str1);
+    
+    int i = 0;
+    while (str1[i] != '\0')
+    {
+        str2[i]=str1[i]+32;
+        i++;
+        }
+    printf("\nconverted lowercase string is: %s", str2);
+    return 0;
+
+}
+
+//___________________________________________________
+
+// lowercase to uppercase conversion without using any inbuild function
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[20];
+    char str2[20];
+    printf("Enter str1: ");
+    gets(str1);
+    
+    int i = 0;
+    while (str1[i] != '\0')
+    {
+        str2[i]=str1[i]-32;
+        i++;
+        }
+    printf("\nlowercase to uppercase conversion is: %s", str2);
+    return 0;
+
+}
+
+//________________________________________________________
