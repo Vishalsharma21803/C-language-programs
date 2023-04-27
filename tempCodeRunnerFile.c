8b15a49c9efcf8bbd9306b9b4a05512ab936d483
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 03:46:06 +0000

    Thursday, April 27, 2023, 9:16 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,23 +1,12 @@
-// strncmp  --- number of characters you want to compare
+// strlen()   --- to finf length of the string 
+// terminating null chaarcter is not including in the counting
 
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char str1[20],str2[20];
-    int n;
-    printf("\nEnter first string: ");
-    gets(str1);
-    printf("\nEnter second string: ");
-    gets(str2);
-    printf("\nEnter the number of characters you want to compare from str2 with complete str1: ");
-    scanf("%d",&n);
-    strncmp(str1,str2,n);  // comparing n characters of str2 with complete str1
-    if(n==0)
-    printf("\nThe two strings are equal");
-    else if(n<0)
-    printf("\nThe second string is greate than the first");
-    else
-    printf("\nThe first string is greater than the second");
+    char str[20];
+    gets(str);
+    printf("the length of the function is: %d",strlen(str));
     return 0;
-    
+
 }
