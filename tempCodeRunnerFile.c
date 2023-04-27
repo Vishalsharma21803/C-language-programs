34cb81f5e3201f03bbc612e0a19a6c1b73c4ce21
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 03:59:38 +0000

    Thursday, April 27, 2023, 9:29 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char str[20];
-    printf("Enter the string in lowercase: ");
-    gets(str);
-    printf("the string in uppercase is: %s",strupr(str));
+    char str1[20]="vishal";
+    char str2[20]="sharma";
+    str2==str1;
+    printf("str1 is: %c",str1);
+    printf("str1 is: %c",str2);
     return 0;
-    
+
 }
