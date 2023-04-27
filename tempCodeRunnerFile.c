0aea10e62bc6b2e5c32fee7b5808f3dcab392d03
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 04:14:17 +0000

    Thursday, April 27, 2023, 9:44 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,14 +2,16 @@
 #include <string.h>
 int main(){
     char str1[20];
-    printf("Enter thr str1: ");
+    char str2[20];
+    printf("Enter str1: ");
     gets(str1);
-    int i=0,temp=0;
-    while(str1[i]!='\0'){
-        temp=temp+1;
-        i++;
-    }
-    printf("\nlength of str1 is: %d",temp);
+    
+    int i = 0;
+    while (str1[i] != '\0')
+    {
+        str2[i]=str1[i]+32;
+        i++}
+    printf("\nstr1: %s", str2);
     return 0;
 
 }
