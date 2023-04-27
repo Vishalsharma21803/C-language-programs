d5847a1a4cde0befd51083746d4355d63459afc5
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 03:49:29 +0000

    Thursday, April 27, 2023, 9:19 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,12 +1,9 @@
-
 #include <stdio.h>
 #include <string.h>
 int main(){
     char str[20];
     printf("Enter the string: ");
     gets(str);
-    printf("the reversed string is: ");
-    strrev(str);
-    printf("%s",str);
+    printf("the reversed string is: %s",strrev(str));
     return 0;
 }
