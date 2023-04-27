7a0aaee7c95e4ba03aee55eaa01c6b9491bf8894
Vishal Sharma <vishal2003ggg@gmail.com>
2023-04-27 04:00:03 +0000

    Thursday, April 27, 2023, 9:30 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,13 @@
+
+
 #include <stdio.h>
 #include <string.h>
 int main(){
     char str1[20]="vishal";
     char str2[20]="sharma";
     str2==str1;
-    printf("str1 is: %c",str1);
-    printf("str1 is: %c",str2);
+    printf("\nstr1 is: %c",str1);
+    printf("\nstr1 is: %c",str2);
     return 0;
 
 }
