bc1ec7dabb2be084f6f60306109141877c7456c0
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-15 12:46:13 +0000

    Monday, May 15, 2023, 6:16 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int a[5],i;
-for(i=0;i<5;i++)
-{
-a[i]=i;
-}
-printf("%d",a[2]);
+int a[5]={11,22,33,44,55};
+a[2]=a[1];
+a[3]=a[2];
+a[4]=a[3];
+printf("%d",a[4]);
 return 0;
 }
