1759d2f3736afcf7a8863d5249e996fe31ed5c59
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-15 10:21:58 +0000

    Monday, May 15, 2023, 3:51 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(){
-float a=123.569854;
-printf("*%0.4f*",a);
-return 0;
+int cubeByValue(int n); // prototype
+int main( void )
+{
+   int number = 5; // initialize number
+   printf("The original value of number is %d", number);
+   cubeByValue(number); // pass number by value
+   printf( "\nThe new value of number is %d\n", number );
+} // end main
+
+int cubeByValue( int n )                                            
+{                                                                   
+   return n*n*n; //cube local variable n and return value
 }
