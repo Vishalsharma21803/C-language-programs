0605d7518a31bc1c64e8e0d55356bd6adc62ef6d
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-15 10:02:16 +0000

    Monday, May 15, 2023, 3:32 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,19 +1,6 @@
-
 #include <stdio.h>
-#include <string.h>
 int main(){
-    char str1[20];
-    char str2[20];
-    printf("Enter str1: ");
-    gets(str1);
-    
-    int i = 0;
-    while (str1[i] != '\0')
-    {
-        str2[i]=str1[i]-32;
-        i++;
-        }
-    printf("\n lowercase to uppercase conversion: %s", str2);
-    return 0;
-
+int a=123;
+printf("*%06d*",a);
+return 0;
 }
