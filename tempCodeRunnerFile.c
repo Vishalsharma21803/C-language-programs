d4c50a473fd88202010c32b8dc79818a806caab2
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-16 08:59:59 +0000

    Tuesday, May 16, 2023 at 2:29 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,42 +1,10 @@
-#include <stdio.h>
-int main()
-{
-    int a[50], n, loc = -1, key, beg, last, mid, i;
-    printf("\n Enter number of array elements:");
-    scanf("%d", &n);
-    printf("\n Enter array elements:");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-    beg = 0;
-    last = n - 1;
-    printf("Enter integer value to search in sorted array:");
-    scanf("%d", &key);
-    while (beg <= last) // Loop will run until unless only one element is not remaining
-    {
-        mid = (beg + last) / 2; // determine index of middle element
-        if (a[mid] == key)
-        {
-            loc = mid; // save the location of element.
-            break;
-        }
-        else if (a[mid] > key) // Middle element is greater than key
-        {
-            last = mid - 1; // If middle element is greater than key, we need to search left subarray
-        }
-        else if (a[mid] < key) // Middle element is less than key
-        {
-            beg = mid + 1; // If middle element is less than key, we need to search right subarray
-        }                  // end of if else
-    }                      // end of while
-    if (loc != -1)
-    {
-        printf("element found at %d", loc + 1); // Location is exact position, not index
-    }
-    else
-    {
-        printf("element not found");
-    }
+#include<stdio.h>
+#define True 1
+#define False 0
+int main(){
+    int a = 0, b = 1;
+    int c;
+    c = True;
+    printf("%d",c);
     return 0;
 }
