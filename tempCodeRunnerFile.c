759a2a091310f6befc7987dd04037fd31e0feb31
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-16 10:04:21 +0000

    Tuesday, May 16, 2023 at 3:34 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main()
-{
- char x = 'z'-'a'; 
- printf("%d",x); /* Display Result = ↓ (difference 
-between ASCII of z and a ) */
+#include <string.h>
+int main(){
+    char ori[20],dup[20];
+    char *z;
+    printf("\nEnter your name: ");
+    gets(ori);
+    printf("\nEnter your name again: ");
+    gets(dup);
+    strcpy(ori,dup); // original ki value ko overright kar kar rahe h duplicate ki value se. original ki value duplicate ki value ke same ho jayegi
+    printf("\nThe original string now is: %s",ori);
+    printf("\nThe duplicate string is: %s",dup);
+    // printf("\nThe value of z is: %s",z);
+    return 0;
 }
