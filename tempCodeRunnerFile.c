274a6d892e30367540605f39b2eedeac41fa08bf
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-19 14:47:38 +0000

    Friday, May 19, 2023 at 8:17 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,18 +1,6 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
 int main(){
-    char str1[20],str2[20];
-    int x;
-    printf("\nEnter first string: ");
-    gets(str1);
-    printf("\nEnter second string: ");
-    gets(str2);
-    x=strcmp(str1,str2);  // comparing the two strings
-    if(x==0)
-    printf("\nThe two strings are equal");
-    else if(x<0)
-    printf("\nThe second string is greate than the first");
-    else
-    printf("\nThe first string is greater than the second");
+    char*str="hello, world\n";
+    printf("%d",strlrn(str));
     return 0;
 }
