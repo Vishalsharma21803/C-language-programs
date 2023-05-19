328858d7d547eb8814d82033cb8e3ff3204cc4cf
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-19 17:30:01 +0000

    Friday, May 19, 2023 at 11:00 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
- void main()
- {
- int k = 8;
- int m = 7;
- k < m ? k = k + 1 : m = m + 1;
- printf("%d", k);
- }
+ int main(){
+ int a = 1, b = 1;
+ switch (a){
+ case a*b:
+ printf("yes ");
+ case a-b:
+ printf("no\n");
+ break;
+ } }
