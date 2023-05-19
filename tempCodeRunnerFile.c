836c34a2d2bb4df5700f66726096a4d5f794bf9a
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-19 14:54:34 +0000

    Friday, May 19, 2023 at 8:24 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include <string.h>
 int main(){
-    char*str="hello, world\n";
-    printf("%d",strlen(str));
+    char *p=NULL;
+    char *q=0;
+    if(p)
+    printf("p is not null\n");
+    else
+    printf("nullp\n");
+    if(q)
+    printf("q is not null\n");
+    else
+    printf("nullq\n");
     return 0;
 }
