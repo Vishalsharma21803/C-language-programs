f469d4742a88a8b38c0f212e3236d3d4dd8f06f2
Vishal Sharma <vishal2003ggg@gmail.com>
2023-05-19 17:38:59 +0000

    Friday, May 19, 2023 at 11:08 PM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
- int main(){
- int a = 1, b = 1;
- switch (a){
- case a*b:
- printf("yes ");
- case a-b:
- printf("no\n");
- break;
- } }
+ int main()
+ {
+ extern ary1[];
+ printf("scope rules\n");
+ }
