b267107ca31b3ac753c601bcae75342879361076
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-03 13:46:01 +0000

    Thursday, August 3, 2023 at 7:16 PM India Standard Time

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,26 @@
-#include <<math.h>
+#include <iostream>
+using namespace std;
 int main(){
-    int a=-50;
-    a=abs(a);
-    printf("%d",a);
-    return 0;
+	int x;
+	int count=0;
+	cout<<"enter a number: ";
+	cin>>x;
+	for(int i=0;i<x;i++){
+		if (x%i==0){
+			count=count+1;
+		}
+		
+	}
+	if (count==0){
+		cout<<"prime number"<<endl;
+		
+	}
+	else{
+		cout<<"not a prime number"<<endl;
+	}
+	
+	
+	
+	
+	return 0;
 }
